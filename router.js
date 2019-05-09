bdf13ca1df8a081b1da710d76b8699fa0bc2e8c9
zzzhezheyo <810884502@qq.com>
2019-05-09 22:06:49 +0800

    feat:用户、商品管理

--- router.js
+++ router.js
@@ -4,14 +4,26 @@ const service = require('./service.js');
 //渲染主页,路由处理
 router.get('/',service.showIndex);
 //添加商品（跳转到添加商品的页面）
+router.get('/toAddUser',service.toAddUser);
+// 添加商品（提交表单）
+router.post('/addUser',service.addUser);
+// 跳转到编辑用户信息页面
+router.get('/toEditUser',service.toEditUser);
+// 编辑用户提交表单
+router.post('/editUser',service.editUser);
+// 删除用户
+router.get('/toDeleteUser',service.toDeleteUser);
+// 商品管理
+router.get('/toProduct',service.toProduct);
+// 上架商品
 router.get('/toAddProduct',service.toAddProduct);
 // 添加商品（提交表单）
 router.post('/addProduct',service.addProduct);
 // 跳转到编辑商品信息页面
-router.get('/toEditPro',service.toEditPro);
+router.get('/toEditProduct',service.toEditProduct);
 // 编辑商品提交表单
-router.post('/editPro',service.editPro);
-// 删除用户
-router.get('/toDeletePro',service.toDeletePro);
+router.post('/editProduct',service.editProduct);
+// 删除商品
+router.get('/toDeleteProduct',service.toDeleteProduct);
 
 module.exports = router;
--- service.js
+++ service.js
@@ -4,35 +4,41 @@ const path = require('path');
 const fs = require('fs');
 const db = require('./db.js');
 
-// 自动生成商品的编号
-let maxProductCode = ()=>{
+// 自动生成用户的编号
+let maxUserCode = ()=>{
 	let arr =[];
 	data.forEach((item)=>{
 		arr.push(item.id);
 	});
 	return Math.max.apply(null,arr);
 }
-
-//渲染主页面
-exports.showIndex = (req,res) =>{
+// //渲染首页
+// exports.showIndex = (req,res) =>{
+// 	let sql = "select * from userinfo"
+// 	db.base(sql,null,(result)=>{
+// 		res.render("index",{list:result});
+// 	});
+// }
+//跳转到用户管理界面
+exports.showIndex = (req,res) => {
 	let sql = "select * from userinfo"
 	db.base(sql,null,(result)=>{
 		res.render("index",{list:result});
 	});
 }
-//调转到添加图书的页面
-exports.toAddProduct = (req,res) => {
-	res.render("addProduct",{});
+//调转到添加用户的页面
+exports.toAddUser = (req,res) => {
+	res.render("addUser",{});
 }
-// 添加商品保存数据
-exports.addProduct = (req,res) => {
+// 添加用户保存数据
+exports.addUser = (req,res) => {
 	let info = req.body;
-	let product ={};
+	let user ={};
 	for(let key in info){
-		product[key] = info[key];
+		user[key] = info[key];
 	}
 	let sql = 'insert into userinfo set ?';
-	db.base(sql,product,(result)=>{
+	db.base(sql,user,(result)=>{
 		if(result.affectedRows == 1){
 			res.redirect('/');
 		}
@@ -40,16 +46,16 @@ exports.addProduct = (req,res) => {
 }
 
 // 跳转编辑页面
-exports.toEditPro=(req,res) =>{
+exports.toEditUser=(req,res) =>{
 	let id = req.query.id;
 	let sql = "select * from userinfo where id=?";
 	let data = [id];
 	db.base(sql,data,(result)=>{
-		res.render('editPro',result[0]);
+		res.render('editUser',result[0]);
 	})
 }
-//编辑商品更新数据
-exports.editPro=(req,res) =>{
+//编辑用户更新数据
+exports.editUser=(req,res) =>{
 	let info = req.body;
 	let sql = 'update userinfo set username=?,tel=?,password=? where id=?';
 	let data =[info.username,info.tel,info.password,info.id];
@@ -60,7 +66,7 @@ exports.editPro=(req,res) =>{
 	})
 }
 // 删除用户信息
-exports.toDeletePro = (req,res)=>{
+exports.toDeleteUser = (req,res)=>{
 	let id = req.query.id;
 	let sql = 'delete from userinfo where id=?';
 	let data = [id];
@@ -71,7 +77,59 @@ exports.toDeletePro = (req,res)=>{
 	})
 }
 
-
-
-
-
+// 跳转商品页面
+exports.toProduct = (req,res) => {
+	let sql = "select * from product"
+	db.base(sql,null,(result)=>{
+		res.render("product",{list:result});
+	});
+}
+// 商品添加界面
+exports.toAddProduct = (req,res) => {
+	res.render("addProduct",{});
+}
+// 添加商品保存数据
+exports.addProduct = (req,res) => {
+	let info = req.body;
+	let product ={};
+	for(let key in info){
+		product[key] = info[key];
+	}
+	let sql = 'insert into product set ?';
+	db.base(sql,product,(result)=>{
+		if(result.affectedRows == 1){
+			res.redirect('/toProduct');
+		}
+	});
+}
+// 跳转编辑页面
+exports.toEditProduct=(req,res) =>{
+	let id = req.query.id;
+	let sql = "select * from product where id=?";
+	let data = [id];
+	db.base(sql,data,(result)=>{
+		res.render('editProduct',result[0]);
+	})
+}
+//编辑商品更新数据
+exports.editProduct=(req,res) =>{
+	let info = req.body;
+	let sql = 'update product set name=?,type=?,price=? where id=?';
+	let data =[info.name,info.type,info.price,info.id];
+	db.base(sql,data,(result)=>{
+		if(result.affectedRows == 1){
+			res.redirect('/toProduct');
+		}
+	})
+}
+// 删除商品信息
+exports.toDeleteProduct = (req,res)=>{
+	let id = req.query.id;
+	let sql = 'delete from product where id=?';
+	let data = [id];
+	db.base(sql,data,(result)=>{
+		if(result.affectedRows == 1){
+			res.redirect('/toProduct');
+		}
+	})
+}
